066d7745dfdf8a765484f0ee2ef80636fed6b73c
Aleksey Ivantsov <Lexin1n3@gmail.com>
2021-01-17 19:00:45 +0500

    PostManager added

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,79 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+
+    private int ownerId;
+    private int postId;
+    private String domain;
+    private String query;
+    private boolean ownersOnly;
+    private int count;
+    private int offset;
+
+
+    public Post[] search() {
+        return null;
+    }
+
+    public void delete() {
+
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public boolean isOwnersOnly() {
+        return ownersOnly;
+    }
+
+    public void setOwnersOnly(boolean ownersOnly) {
+        this.ownersOnly = ownersOnly;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+}
